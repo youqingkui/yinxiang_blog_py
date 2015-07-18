99c131eafd45ef0c61ec4671b41738dd0b7f3fb9
youqingkui <youqingkui@qq.com>
2015-07-18 11:13:00 +0800

    测试印象笔记连连

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from flask import render_template, session, redirect, url_for, current_app
 from . import main
 from app.models import User
+from evernote.api.client import EvernoteClient
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,3 +15,17 @@ def index():
     u.last_name = "kui"
     u.save()
     return  "ok"
+
+
+@main.route('/yx')
+def yx():
+    """
+    测试印象笔记服务
+    :return:
+    """
+    client = EvernoteClient(token=dev_token,sandbox=False)
+    client.service_host = 'app.yinxiang.com'
+    userStore = client.get_user_store()
+    user = userStore.getUser()
+    print user
+    return "yx"
